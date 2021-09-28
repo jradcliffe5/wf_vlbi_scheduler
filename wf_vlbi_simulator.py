df76ca200956acb4ae3d9ae60115d650faf14266
Jack Radcliffe <jackradcliffe@MacBook-Pro.local>
2021-09-28 11:11:32 +0200

    added noise

--- wf_vlbi_simulator.py
+++ wf_vlbi_simulator.py
@@ -56,7 +56,7 @@ commands.append('%s simulations/make_itrf.py %s'%(inputs['CASA_exec']," ".join(a
 commands.append('%s simulations/make_measurement_set.py single simulator_inputs.txt'%(inputs['stimela_exec']))
 
 ## Add noise to measurement sets & flag
-commands.append('%s simulations/add_noise_hetero.py %s/single_pointing.ms %d'%(inputs['CASA_exec'],inputs['output_path'],inputs['size']))
+commands.append('%s simulations/add_noise_hetero.py %s/single_pointing.ms %d'%(inputs['CASA_exec'],inputs['output_path'],int(inputs['size'])))
 
 with open('job_%s.%s'%(step,params['job_manager']), 'a') as filehandle:
 	for listitem in commands:
