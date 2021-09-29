2e10ff2bc006070607a71f2bd97d68495a0d1c9b
Jack Radcliffe <jackradcliffe@MacBook-Pro.local>
2021-09-29 10:17:51 +0200

    changes

--- simulations/convert_fits_to_casa.py
+++ simulations/convert_fits_to_casa.py
@@ -7,5 +7,5 @@ import sys
 
 ms = sys.argv[1]
 
-importfits(fitsimage='%s_image-beam-I.fits'%ms,imagename="%s.pb"%ms)
-importfits(fitsimage='%s_image-image-pb.fits'%ms,imagename='%s_pb.image'%ms)
+importfits(fitsimage='%s_IM-beam-I.fits'%ms,imagename="%s.pb"%ms)
+importfits(fitsimage='%s_IM-image-pb.fits'%ms,imagename='%s_pb.image'%ms)
