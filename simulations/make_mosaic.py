10fc5335ccbedd72afef6f070e95da7cf12800ab
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2021-10-05 10:06:45 +0200

    test

--- simulations/make_mosaic.py
+++ simulations/make_mosaic.py
@@ -15,15 +15,18 @@ for i in os.listdir('%s'%inputs['output_path']):
 		pbs.append("%s/%s"%(inputs['output_path'],i))
 	elif i.endswith('.ms_pb.image'):
 		ims.append("%s/%s"%(inputs['output_path'],i))
+ims.sort()
+pbs.sort()
 
 lm = casatools.linearmosaic()
-os.system('rm -r test.linmos test.weightlinmos')
+os.system('rm -r %smosaic.linmos %smosaic.weightlinmos'%(inputs['output_path'],inputs['output_path']))
 print('Defining output image')
-imsize = np.array(ast.literal_eval(inputs['mosaic_area']))*1.2*3.6e6
+imsize = np.array(ast.literal_eval(inputs['mosaic_area']))*1.2*3.6e3
 max_mosaic_size = 7200
 cell = '%.5farcsec'%(imsize/max_mosaic_size)[0]
+print(cell)
 pc = ast.literal_eval(inputs['field_centre'])
-lm.defineoutputimage(nx=max_mosaic_size, cellx=cell, imagecenter='%s %s'%(pc[0],pc[1]), outputimage='%s/mosaic.linmos'%inputs['output_path'], outputweight='%s/mosaic.weightlinmos'%inputs['output_path'])
+lm.defineoutputimage(nx=max_mosaic_size, cellx=cell,ny=max_mosaic_size, celly=cell, imagecenter='%s %s'%(pc[0],pc[1]), outputimage='%s/mosaic.linmos'%inputs['output_path'], outputweight='%s/mosaic.weightlinmos'%inputs['output_path'])
 print('Making linear mosaic')
 for i in range(len(ims)):
 	print(i)
