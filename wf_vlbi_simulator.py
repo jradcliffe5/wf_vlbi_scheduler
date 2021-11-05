60d22ba39d09d8cd488936f69a116bbbfe023f6b
Jack Radcliffe <jackradcliffe@MacBook-Pro.local>
2021-11-05 10:32:18 +0200

    removed defunct

--- wf_vlbi_simulator.py
+++ wf_vlbi_simulator.py
@@ -37,15 +37,7 @@ params['max_jobs'] = int(inputs['max_jobs'])
 
 ## Set the parameters for software
 print(inputs['CASA_exec'])
-'''
-singularity exec /idia/software/containers/casa-6.3.simg python make_itrf.py evn emerlin
-singularity exec ../STIMELA_SINGULARITY_IMAGES/stimela_simms_1.2.0.img python make_measurement_set.py single evn emerlin
-singularity exec /idia/software/containers/casa-6.3.simg python add_noise_hetero.py evn_emerlin.ms 2048
-singularity exec /idia/software/containers/casa-6.3.simg python generate_pb_aterms.py evn_emerlin.ms 0 0 0 
-gunzip "evn_emerlin.ms_pb_flat_norotate.fits.gz"
-singularity exec /idia/software/containers/wsclean-gpu.simg wsclean -name evn_emerlin_nat -no-update-model-required --aterm-kernel-size 157 -weight natural -scale 1asec -niter 1 -mgain 0.9 -auto-threshold 0.5 -auto-mask 4 -use-idg -idg-mode hybrid -aterm-config evn_emerlin.ms_aterm_norotate_config.txt -size 2048 2048 evn_emerlin.ms
-singularity exec /idia/software/containers/wsclean-gpu.simg wsclean -name evn_emerlin_uni -no-update-model-required --aterm-kernel-size 157 -weight uniform -scale 1asec -niter 1 -mgain 0.9 -auto-threshold 0.5 -auto-mask 4 -use-idg -idg-mode hybrid -aterm-config evn_emerlin.ms_aterm_norotate_config.txt -size 2048 2048 evn_emerlin.ms
-'''
+
 ## Generate single pointing to fit beam
 if part == 1:
 	commands = []
@@ -124,15 +116,3 @@ if part==2:
 		with open('job_%s.%s'%(step,params['job_manager']), 'a') as filehandle:
 			for listitem in commands:
 				filehandle.write('%s\n' % listitem)
-	'''
-	array=(mosaic_ms/*.ms)
-	len=${#array[@]}
-	a=$SLURM_ARRAY_TASK_ID
-
-	singularity exec /idia/software/containers/casa-6.3.simg python add_noise_hetero.py ${array[$a]} 2048
-	singularity exec /idia/software/containers/casa-6.3.simg python generate_pb_aterms.py ${array[$a]} 0 0 0 
-	gunzip ${array[$a]}"_pb_flat_norotate.fits.gz"
-	singularity exec /idia/software/containers/wsclean-gpu.simg wsclean -name ${array[$a]}_image -no-update-model-required --aterm-kernel-size 157 -weight natural -scale 1asec -niter 1 -mgain 0.9 -auto-threshold 0.5 -auto-mask 4 -use-idg -idg-mode hybrid -aterm-config ${array[$a]}_aterm_norotate_config.txt -size 2048 2048 ${array[$a]}
-	singularity exec /idia/software/containers/casa-6.3.simg python convert_fits_to_casa.py ${array[$a]}
-	rm casa*log
-	'''
